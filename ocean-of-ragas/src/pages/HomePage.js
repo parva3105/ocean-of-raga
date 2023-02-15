949391c1938d8825072e9943e31522f92f244ebb
Parva Shah <parvashah310501@gmail.com>
2023-02-15 13:47:22 -0500

    Homepage Changes

--- ocean-of-ragas/src/pages/HomePage.js
+++ ocean-of-ragas/src/pages/HomePage.js
@@ -9,8 +9,6 @@ import Nav from '../ComponentsHome/NavBar/Nav'
 function HomePage() {
   return (
     <div className="App">
-      {/* <Header />
-      <About /> */}
       <Header />
       <About />
       <Nav />
