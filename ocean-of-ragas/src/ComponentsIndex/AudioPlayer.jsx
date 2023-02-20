b2a3d48f4aed5cc7df18c422751ac5ecb180602f
Parva Shah <parvashah310501@gmail.com>
2023-02-20 04:00:00 -0500

    Issue with Audio player

--- ocean-of-ragas/src/ComponentsIndex/AudioPlayer.jsx
+++ ocean-of-ragas/src/ComponentsIndex/AudioPlayer.jsx
@@ -1,20 +1,20 @@
-import React, { useState,useEffect } from 'react';
-import defaultSong from '../Assets/songs/songs_Adbhutranjani_GoswamiGokulotsavmaharaj.mp3'
+import React from 'react';
+// import defaultSong from '../Assets/songs/songs_Adbhutranjani_GoswamiGokulotsavmaharaj.mp3'
 
 const AudioPlayer = ({path}) => {
 
 
-  const [pathName,setPathName] = useState(defaultSong) //  This Works        
+  // const [pathName,setPathName] = useState(path) //  This Works        
   // const [pathName,setPathName] = useState(path) //  This Won't Works        
   
-
+  console.log(path);
 
   return (
     <div className='header__player'>
         <div id='now-playing' className=" info header__audio">Now Playing : 5_Hindol.mp3  </div>
             <div id='audio-tag' className="header__player__control">
             <audio id='my-audio' autoPlay controls>
-                <source id='audio-source' src={pathName} type="audio/mp3"/>
+                <source id='audio-source' src={path} type="audio/mp3"/>
             </audio>
         </div>
     </div>
