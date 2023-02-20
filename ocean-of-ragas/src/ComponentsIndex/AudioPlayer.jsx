e85a8fc7ba36d591f5ef45c9c640c31ccfc1ba37
Parva Shah <parvashah310501@gmail.com>
2023-02-19 23:15:19 -0500

    setting up audio player

--- ocean-of-ragas/src/ComponentsIndex/AudioPlayer.jsx
+++ ocean-of-ragas/src/ComponentsIndex/AudioPlayer.jsx
@@ -2,14 +2,22 @@ import React, { useState } from 'react';
 import defaultSong from '../Assets/songs/songs_Adbhutranjani_GoswamiGokulotsavmaharaj.mp3'
 
 const AudioPlayer = ({path}) => {
-  const pathName = defaultSong
+  
+  const [pathName,setPathName] = useState(defaultSong)
+  
+
+  if(path !== defaultSong){
+    console.log(path);
+  }
+  
+  
   
   return (
     <div className='header__player'>
         <div id='now-playing' className=" info header__audio">Now Playing : 5_Hindol.mp3  </div>
             <div id='audio-tag' className="header__player__control">
             <audio id='my-audio' autoPlay controls>
-                <source id='audio-source' src={pathName} type="audio/mp3"/>
+                <source id='audio-source' src={pathName} type="audio/mpeg"/>
             </audio>
         </div>
     </div>
