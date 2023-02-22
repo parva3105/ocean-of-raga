a20d68993503dee7fb17d16d7468252acd29a9b1
Parva Shah <parvashah310501@gmail.com>
2023-02-22 05:14:57 -0500

    Optimized Content Code

--- ocean-of-ragas/src/ComponentsHome/Content/Content.jsx
+++ ocean-of-ragas/src/ComponentsHome/Content/Content.jsx
@@ -1,59 +1,75 @@
-import React, { useContext } from 'react'
-import './content.css'
-import { GiSpeaker } from 'react-icons/gi'
-import { SongContext } from '../../context/SongContext'
+import React, { useContext } from 'react';
+import './content.css';
+import { GiSpeaker } from 'react-icons/gi';
+import { SongContext } from '../../context/SongContext';
+
+const songs = [
+  {
+    name: 'Asavri',
+    artist: 'T.K.Janorikar',
+    instrument: '-',
+    path: 'Asavri'
+  },
+  {
+    name: 'Abheri',
+    artist: 'Rajesh Vaidya',
+    instrument: 'Veena',
+    path: 'Abheri'
+  },
+  {
+    name: 'AbheriTodi',
+    artist: 'Ramashreya Jha',
+    instrument: '-',
+    path: 'AbheriTodi'
+  },
+  {
+    name: 'Abhogi',
+    artist: 'Rajashri Mahajani',
+    instrument: '-',
+    path: 'Abhogi'
+  },
+];
 
 const Content = () => {
+  const { setPath } = useContext(SongContext);
 
-  const { setPath } = useContext(SongContext)
-
-  function setSong(e) {
-    setPath(e.target.value)
-  }
+  const handleSongClick = (path) => {
+    setPath(path);
+  };
 
   return (
-    <div>
-      <div className='content__table'>
-        <table className='table__head'>
-          <thead>
+    <div className='content__table'>
+      <table className='table__head'>
+        <thead>
           <tr>
             <th>Name</th>
             <th>Artist</th>
             <th>Instrument</th>
             <th>Play</th>
           </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <td>Asavri</td>
-            <td>T.K.Janorikar</td>
-            <td>-</td>
-            <td><button id='songBtn asavri' value='Asavri' onClick={setSong} className='player'>Play <GiSpeaker/></button></td>
-          </tr>
-          <tr>
-            <td>Abheri</td>
-            <td>Rajesh Vaidya</td>
-            <td>Veena</td> 
-            <td><button id='songBtn abheri' value='Abheri' onClick={setSong} className='player'>Play <GiSpeaker/></button></td>
-          </tr>
-          <tr>
-            <td>AbheriTodi</td>
-            <td>Ramashreya Jha</td>
-            <td>-</td>
-            <td><button id='songBtn abheritodi' value='AbheriTodi' onClick={setSong} className='player'>Play <GiSpeaker/></button></td>
-          </tr>
-          <tr>
-            <td>Abhogi</td>
-            <td>Rajashri Mahajani</td>
-            <td>-</td>
-            <td><button id='songBtn abhogi' value='Abhogi' onClick={setSong} className='player'>Play <GiSpeaker/></button></td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-      
+        </thead>
+        <tbody>
+          {songs.map((song) => (
+            <tr key={song.path}>
+              <td>{song.name}</td>
+              <td>{song.artist}</td>
+              <td>{song.instrument}</td>
+              <td>
+                <button
+                  id={`songBtn ${song.path}`}
+                  value={song.path}
+                  onClick={() => handleSongClick(song.path)}
+                  className='player'
+                >
+                  Play <GiSpeaker />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
